56c0cfae7cafed5ac4a095dfa8c504075f6b6fae
Mohamad Mando <homssword@gmail.com>
2026-04-29 12:57:44 +0200

    add: utils.py which combines each train .ply file with its corresponding .lb file

---
+++ utils.py
@@ -0,0 +1,56 @@
+import numpy as np
+import numpy.lib.recfunctions as recfun
+from plyfile import PlyData, PlyElement
+from pathlib import Path
+
+
+# path definition
+ply_dir = Path('./Challenge-ABC/Train/ply')
+lb_dir = Path('./Challenge-ABC/Train/lb')
+output_dir = Path('./Challenge-ABC/Train/ply_labeled')
+output_dir.mkdir(parents=True, exist_ok=True) # create the output folder if it doesn't exist
+
+# we loop over the ply files
+for ply_file in ply_dir.glob('*.ply'):
+
+    # extract the prefix/file name
+    file_id = ply_file.stem
+
+    # define the .lb file path
+    lb_file = lb_dir / f"{file_id}.lb"
+
+    # check that the label file exists
+    if not lb_file.exists():
+        print(f"Error : Didn't find label file for {file_id}.ply -> ignored.")
+        continue
+    
+    print(f"Prcessing: {file_id}.ply")
+    ply_data = PlyData.read(ply_file)           # read .ply content
+    labels = np.loadtxt(lb_file, dtype='int')   # read .lb content
+    vertex_data = ply_data.elements[0].data     # extract vertex data
+
+    # check that nb_vertex == nb_labels, if not ignore it
+    if len(vertex_data) != len(labels):         
+        print(f"Dimension error with {file_id} : {len(vertex_data)} points (.ply) vs {len(labels)} labels (.lb) -> ignored.")
+        continue
+
+    # create the new vertex_data which appends labels field to already existing fields  
+    new_vertex_data = recfun.append_fields(
+        vertex_data, 
+        names='label', 
+        data=labels, 
+        dtypes='i4', 
+        usemask=False
+    )
+
+    # create the new Ply element to transform it after 
+    new_vertex_element = PlyElement.describe(new_vertex_data, 'vertex')
+
+    # write the new file
+    output_path = output_dir / f"{file_id}_labeled.ply"
+    PlyData([new_vertex_element]).write(str(output_path))   
+
+print(f"OK.")
+
+
+

2b885f622fcfb0572f5d8815cd6ea9f91992ba90
Mohamad Mando <homssword@gmail.com>
2026-04-29 17:49:28 +0200

    feat: add extract_stats function

--- utils.py
+++ utils.py
@@ -61,9 +61,107 @@ def combine_labels(dataset='Train'):
 
     print(f"OK.")
 
+def extract_stats(dataset='Train'):
+    # path definition
+    # base directory for the chosen dataset
+    base_path = Path('./Challenge-ABC') / dataset
+    if not base_path.exists(): # verification
+        print(f"Error: Dataset folder {base_path} not found, check 'dataset' argument.")
+        return
+
+    # dynamic definition
+    ply_dir = base_path / 'ply'
+    lb_dir = base_path / 'lb'
+  
+    # to store each file data
+    ids = []
+    points_counts = []
+    edges_counts = []
+
+    # we loop over the ply files
+    for ply_file in ply_dir.glob('*.ply'):
+
+        # extract the prefix/file name
+        file_id = ply_file.stem
+
+        # define the .lb file path
+        lb_file = lb_dir / f"{file_id}.lb"
+
+        # check that the label file exists
+        if not lb_file.exists():
+            print(f"Error : Didn't find label file for {file_id}.ply -> ignored.")
+            continue
+        
+        #print(f"Prcessing: {file_id}.ply")
+        ply_data = PlyData.read(ply_file)           # read .ply content
+        labels = np.loadtxt(lb_file, dtype='int')   # read .lb content
+        vertex_data = ply_data.elements[0].data     # extract vertex data
+
+        # check that nb_vertex == nb_labels, if not ignore it
+        if len(vertex_data) != len(labels):         
+            print(f"Dimension error with {file_id} : {len(vertex_data)} points (.ply) vs {len(labels)} labels (.lb) -> ignored.")
+            continue
+        
+        ids.append(file_id)
+        points_counts.append(len(labels))
+        edges_counts.append(np.sum(labels))
+
+    # convert to numpy arrays 
+    points_arr = np.array(points_counts)
+    edges_arr = np.array(edges_counts)
+
+    # calculate metrics
+    percentages = (edges_arr / points_arr) * 100
+
+    idx_max_p = np.argmax(points_arr)
+    idx_min_p = np.argmin(points_arr)
+    idx_max_pct = np.argmax(percentages)
+    idx_min_pct = np.argmin(percentages)
+
+    total_points = np.sum(points_arr)
+    mean_points = np.mean(points_arr, dtype='int')
+
+    total_edges = np.sum(edges_arr)
+    global_edge_pct = (total_edges / total_points) * 100
+
+    stats_summary = {
+        "total_points": total_points,
+        "total_edges": total_edges,
+        "mean_points" : mean_points,
+        "global_edge_pct": global_edge_pct,
+        "max_points": (ids[idx_max_p], points_arr[idx_max_p]),
+        "min_points": (ids[idx_min_p], points_arr[idx_min_p]),
+        "highest_pct": (ids[idx_max_pct], percentages[idx_max_pct]),
+        "lowest_pct": (ids[idx_min_pct], percentages[idx_min_pct])
+    }
+    
+    return stats_summary
+
 combine_labels()
 combine_labels('Validation')
 #combine_labels('Test') => Error: Dataset folder Challenge-ABC/Test not found, check 'dataset' argument.
+
+train_stats_summary = extract_stats()
+print("---Train Dataset---")
+print(f"Total points = {train_stats_summary['total_points']} points.")
+print(f"Mean points = {train_stats_summary['mean_points']} points.")
+print(f"Total edges = {train_stats_summary['total_edges']} points.")
+print(f"Global edge percentage = {train_stats_summary['global_edge_pct']:.2f}%.")
+print(f"Model with max points = {train_stats_summary['max_points'][0]}.ply. Has {train_stats_summary['max_points'][1]} points.")
+print(f"Model with min points = {train_stats_summary['min_points'][0]}.ply. Has {train_stats_summary['min_points'][1]} points.")
+print(f"Model with the highest edge points percentage = {train_stats_summary['highest_pct'][0]}.ply. Has {train_stats_summary['highest_pct'][1]:.2f}%.")
+print(f"Model with the lowest edge points percentage = {train_stats_summary['lowest_pct'][0]}.ply. Has {train_stats_summary['lowest_pct'][1]:.2f}%.")
+
+validation_stats_summary = extract_stats('Validation')
+print("\n---Validation Dataset---")
+print(f"Total points = {validation_stats_summary['total_points']} points.")
+print(f"Mean points = {validation_stats_summary['mean_points']} points.")
+print(f"Total edges = {validation_stats_summary['total_edges']} points.")
+print(f"Global edge percentage = {validation_stats_summary['global_edge_pct']:.2f}%.")
+print(f"Model with max points = {validation_stats_summary['max_points'][0]}.ply. Has {validation_stats_summary['max_points'][1]} points.")
+print(f"Model with min points = {validation_stats_summary['min_points'][0]}.ply. Has {validation_stats_summary['min_points'][1]} points.")
+print(f"Model with the highest edge points percentage = {validation_stats_summary['highest_pct'][0]}.ply. Has {validation_stats_summary['highest_pct'][1]:.2f}%.")
+print(f"Model with the lowest edge points percentage = {validation_stats_summary['lowest_pct'][0]}.ply. Has {validation_stats_summary['lowest_pct'][1]:.2f}%.")
 
 
 

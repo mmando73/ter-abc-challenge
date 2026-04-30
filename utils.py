e47d92fcad2e0bb32963e223bb0df7f97f7a948e
Mohamad Mando <homssword@gmail.com>
2026-04-30 17:05:53 +0200

    feat: merge "combine_labels" and "extract_stats" into one function

--- utils.py
+++ utils.py
@@ -4,144 +4,94 @@ from plyfile import PlyData, PlyElement
 from pathlib import Path
 
 
-def combine_labels(dataset='Train'):
-    # path definition
+def process_dataset(dataset='Train', save_ply=False):
+    # 1) Path Definition
     # base directory for the chosen dataset
     base_path = Path('./Challenge-ABC') / dataset
     if not base_path.exists(): # verification
         print(f"Error: Dataset folder {base_path} not found, check 'dataset' argument.")
-        return
+        return None
 
-    # dynamic definition
+    # dynamic definition for the .ply and .lb files
     ply_dir = base_path / 'ply'
     lb_dir = base_path / 'lb'
-    output_dir = base_path / 'ply_labeled'
+    if save_ply:
+        output_dir = base_path / 'ply_labeled'
+        output_dir.mkdir(parents=True, exist_ok=True) # create the output folder if it doesn't exist
 
-    output_dir.mkdir(parents=True, exist_ok=True) # create the output folder if it doesn't exist
-
-    # we loop over the ply files
-    for ply_file in ply_dir.glob('*.ply'):
-
-        # extract the prefix/file name
-        file_id = ply_file.stem
-
-        # define the .lb file path
-        lb_file = lb_dir / f"{file_id}.lb"
-
-        # check that the label file exists
-        if not lb_file.exists():
-            print(f"Error : Didn't find label file for {file_id}.ply -> ignored.")
-            continue
-        
-        print(f"Prcessing: {file_id}.ply")
-        ply_data = PlyData.read(ply_file)           # read .ply content
-        labels = np.loadtxt(lb_file, dtype='int')   # read .lb content
-        vertex_data = ply_data.elements[0].data     # extract vertex data
-
-        # check that nb_vertex == nb_labels, if not ignore it
-        if len(vertex_data) != len(labels):         
-            print(f"Dimension error with {file_id} : {len(vertex_data)} points (.ply) vs {len(labels)} labels (.lb) -> ignored.")
-            continue
-
-        # create the new vertex_data which appends labels field to already existing fields  
-        new_vertex_data = recfun.append_fields(
-            vertex_data, 
-            names='label', 
-            data=labels, 
-            dtypes='i4', 
-            usemask=False
-        )
-
-        # create the new Ply element to transform it after 
-        new_vertex_element = PlyElement.describe(new_vertex_data, 'vertex')
-
-        # write the new file
-        output_path = output_dir / f"{file_id}_labeled.ply"
-        PlyData([new_vertex_element]).write(str(output_path))   
-
-    print(f"OK.")
-
-def extract_stats(dataset='Train'):
-    # path definition
-    # base directory for the chosen dataset
-    base_path = Path('./Challenge-ABC') / dataset
-    if not base_path.exists(): # verification
-        print(f"Error: Dataset folder {base_path} not found, check 'dataset' argument.")
-        return
-
-    # dynamic definition
-    ply_dir = base_path / 'ply'
-    lb_dir = base_path / 'lb'
-  
-    # to store each file data
+    # 2) Store each file data/stats
     ids = []
     points_counts = []
     edges_counts = []
 
-    # we loop over the ply files
+    # 3) Process loop
     for ply_file in ply_dir.glob('*.ply'):
-
         # extract the prefix/file name
         file_id = ply_file.stem
 
         # define the .lb file path
-        lb_file = lb_dir / f"{file_id}.lb"
+        lb_file = lb_dir /f"{file_id}.lb"
 
-        # check that the label file exists
+        # check that the labels file exists
         if not lb_file.exists():
-            print(f"Error : Didn't find label file for {file_id}.ply -> ignored.")
+            print(f"Error: Didn't find labels file for {file_id}.ply -> ignored.")
             continue
-        
-        #print(f"Prcessing: {file_id}.ply")
-        ply_data = PlyData.read(ply_file)           # read .ply content
-        labels = np.loadtxt(lb_file, dtype='int')   # read .lb content
-        vertex_data = ply_data.elements[0].data     # extract vertex data
 
-        # check that nb_vertex == nb_labels, if not ignore it
+        # reading data
+        #print(f"Prcessing: {file_id}.ply")
+        ply_data = PlyData.read(ply_file)
+        labels = np.loadtxt(lb_file, dtype='int')
+        vertex_data = ply_data.elements[0].data
+
+        # Verification : check that nb_vertex == nb_labels, if not ignore it
         if len(vertex_data) != len(labels):         
             print(f"Dimension error with {file_id} : {len(vertex_data)} points (.ply) vs {len(labels)} labels (.lb) -> ignored.")
             continue
         
+        # Data gathering for stats
         ids.append(file_id)
         points_counts.append(len(labels))
         edges_counts.append(np.sum(labels))
 
-    # convert to numpy arrays 
+        # Optional combining+writing new files logic
+        if save_ply:
+            # create the new vertex_data which appends labels field to already existing fields  
+            new_vertex_data = recfun.append_fields(
+                vertex_data, 
+                names='label', 
+                data=labels, 
+                dtypes='i4', 
+                usemask=False
+            )
+            new_vertex_element = PlyElement.describe(new_vertex_data, 'vertex')
+            # write the new file
+            output_path = output_dir / f"{file_id}_labeled.ply"
+            PlyData([new_vertex_element]).write(str(output_path))
+        
+    # 4) Compute Stats
     points_arr = np.array(points_counts)
     edges_arr = np.array(edges_counts)
 
-    # calculate metrics
+    # avoid division by zero if empty
+    if len(points_arr) == 0: return None
+
     percentages = (edges_arr / points_arr) * 100
-
-    idx_max_p = np.argmax(points_arr)
-    idx_min_p = np.argmin(points_arr)
-    idx_max_pct = np.argmax(percentages)
-    idx_min_pct = np.argmin(percentages)
-
     total_points = np.sum(points_arr)
-    mean_points = np.mean(points_arr, dtype='int')
-
     total_edges = np.sum(edges_arr)
-    global_edge_pct = (total_edges / total_points) * 100
 
-    stats_summary = {
+    return {
         "total_points": total_points,
         "total_edges": total_edges,
-        "mean_points" : mean_points,
-        "global_edge_pct": global_edge_pct,
-        "max_points": (ids[idx_max_p], points_arr[idx_max_p]),
-        "min_points": (ids[idx_min_p], points_arr[idx_min_p]),
-        "highest_pct": (ids[idx_max_pct], percentages[idx_max_pct]),
-        "lowest_pct": (ids[idx_min_pct], percentages[idx_min_pct])
+        "mean_points" : int(np.mean(points_arr)),
+        "global_edge_pct": (total_edges / total_points) * 100,
+        "max_points": (ids[np.argmax(points_arr)], np.max(points_arr)),
+        "min_points": (ids[np.argmin(points_arr)], np.min(points_arr)),
+        "highest_pct": (ids[np.argmax(percentages)], np.max(percentages)),
+        "lowest_pct": (ids[np.argmin(percentages)], np.min(percentages))
     }
-    
-    return stats_summary
 
-combine_labels()
-combine_labels('Validation')
-#combine_labels('Test') => Error: Dataset folder Challenge-ABC/Test not found, check 'dataset' argument.
 
-train_stats_summary = extract_stats()
+train_stats_summary = process_dataset(dataset='Train', save_ply=True)
 print("---Train Dataset---")
 print(f"Total points = {train_stats_summary['total_points']} points.")
 print(f"Mean points = {train_stats_summary['mean_points']} points.")
@@ -152,7 +102,7 @@ print(f"Model with min points = {train_stats_summary['min_points'][0]}.ply. Has 
 print(f"Model with the highest edge points percentage = {train_stats_summary['highest_pct'][0]}.ply. Has {train_stats_summary['highest_pct'][1]:.2f}%.")
 print(f"Model with the lowest edge points percentage = {train_stats_summary['lowest_pct'][0]}.ply. Has {train_stats_summary['lowest_pct'][1]:.2f}%.")
 
-validation_stats_summary = extract_stats('Validation')
+validation_stats_summary = process_dataset(dataset='Validation', save_ply=True)
 print("\n---Validation Dataset---")
 print(f"Total points = {validation_stats_summary['total_points']} points.")
 print(f"Mean points = {validation_stats_summary['mean_points']} points.")
@@ -162,6 +112,8 @@ print(f"Model with max points = {validation_stats_summary['max_points'][0]}.ply.
 print(f"Model with min points = {validation_stats_summary['min_points'][0]}.ply. Has {validation_stats_summary['min_points'][1]} points.")
 print(f"Model with the highest edge points percentage = {validation_stats_summary['highest_pct'][0]}.ply. Has {validation_stats_summary['highest_pct'][1]:.2f}%.")
 print(f"Model with the lowest edge points percentage = {validation_stats_summary['lowest_pct'][0]}.ply. Has {validation_stats_summary['lowest_pct'][1]:.2f}%.")
+
+#process_dataset('Test') => Error: Dataset folder Challenge-ABC/Test not found, check 'dataset' argument.
 
 # Output:
 # ---Train Dataset---
